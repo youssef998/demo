eb6738eddfe76288fb7c4244493feaf1e1d21b65
youssef-mostafa1 <youssef.mostafa1@vodafone.com>
2024-09-21 19:13:48 +0300

    turkey version

--- script.js
+++ script.js
@@ -1,7 +1,59 @@
-const box = document.getElementById('giftBox');
-const lid = document.getElementById('lid');
-const content = document.getElementById('message');
+// const clues = ["clue1", "clue2", "clue3"];
+// let currentClue = 0;
 
-box.addEventListener('click', () => {
-    box.classList.toggle('open');
+// document.addEventListener("DOMContentLoaded", () => {
+//   setTimeout(() => document.getElementById("question0").style.opacity = 1, 1000);   
+//   setTimeout(() => document.getElementById("question1").style.opacity = 1, 4000);
+//   setTimeout(() => document.getElementById("question2").style.opacity = 1, 5000);
+// });
+
+// function revealClues() {
+//   if (currentClue < clues.length) {
+//     document.getElementById(clues[currentClue]).style.opacity = 1;
+//     currentClue++;
+//   } else {
+//     document.getElementById("finalReveal").style.opacity = 1;
+//     document.getElementById("revealBtn").style.display = "none";
+//     startHearts();
+//   }
+// }
+
+// function startHearts() {
+//   const hearts = document.querySelectorAll('.heart');
+//   hearts.forEach((heart, i) => {
+//     setTimeout(() => heart.style.animationPlayState = 'running', i * 1000);
+//   });
+// }
+const clues = ["clue1", "clue2", "clue3"];
+let currentClue = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => document.getElementById("question0").style.opacity = 1, 1000);   
+  setTimeout(() => document.getElementById("question1").style.opacity = 1, 4000);
+  setTimeout(() => document.getElementById("question2").style.opacity = 1, 5000);
 });
+
+function revealClues() {
+  if (currentClue < clues.length) {
+    document.getElementById(clues[currentClue]).style.opacity = 1;
+    currentClue++;
+  }
+
+  if (currentClue === clues.length) {
+    document.getElementById("revealBtn").style.display = "none";
+    document.getElementById("noticeBtn").style.display = "inline-block";
+  }
+}
+
+function revealFinalMessage() {
+  document.getElementById("finalReveal").style.opacity = 1;
+  document.getElementById("noticeBtn").style.display = "none";
+  startHearts();
+}
+
+function startHearts() {
+  const hearts = document.querySelectorAll('.heart');
+  hearts.forEach((heart, i) => {
+    setTimeout(() => heart.style.animationPlayState = 'running', i * 1000);
+  });
+}
